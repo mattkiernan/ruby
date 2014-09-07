5fff8963be598025c7e5dc487dcee9bfa0ac9355
mattkiernan <matt.kiernan@gmail.com>
2014-09-07 10:13:59 -0400

    screwed up mixing.rb and updated methods.rb

--- methods.rb
+++ methods.rb
@@ -1,3 +1,5 @@
-puts 'What do you want?!'
-reply = gets.chomp
-puts 'What do you mean,"' + reply + '"?! You\'re fired!'
+lineWidth = 60
+puts 'Table of Contents'.center lineWidth
+puts 'Chapter 1: Numbers'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2)
+puts 'Chapter 2: Letters'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2)
+puts 'Chapter 3: Variables'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2)
