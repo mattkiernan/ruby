2a285d9f292009a5d9e0b1a1554dc4ae63b505ab
mattkiernan <matt.kiernan@gmail.com>
2014-09-11 16:25:45 -0400

    dong script

--- dong.rb
+++ dong.rb
@@ -1,12 +1,12 @@
 def clock some_proc
 	current_hour = Time.new.hour
 	if current_hour == 0
-		current_hour == 12
+		current_hour = 12
 	elsif current_hour > 12
 		current_hour = current_hour - 12
 	end
 
-	current_hour.to_i.times  do
+	current_hour.to_i.times do
 		some_proc.call
 	end
 end
